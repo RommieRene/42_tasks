393e4d7a93a74c1c7e863bdd901056a6d17a24d2
Rene Giragosyan Namagerdi <rgiragos@c2r12s2.42yerevan.am>
2024-12-06 13:03:08 +0400

    sdad

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -6,24 +6,24 @@
 /*   By: rgiragos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 23:04:39 by rgiragos          #+#    #+#             */
-/*   Updated: 2024/12/05 23:15:45 by rgiragos         ###   ########.fr       */
+/*   Updated: 2024/12/06 12:58:47 by rgiragos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void ft_rev_int_tab(int *tab, int size)
-{
-	int	rev;
-	int i;
-    int count;
-    count = 0;
-    while(size[count] != '\0')
-    {
-        count++;
-    }
-    return (count);
-    rev = *tab;
-    i = count - 0;
- 
+{ 
+	int	start;
+	int	end;
+	int	temp;
 
-
-	
+	start = 0;
+	end = size - 1;
+	while (start < end)
+	{
+		temp = tab[start];
+		tab[start] = tab[end];
+		tab [end] = temp;
+		start++;
+		end--;
+	}
+}
